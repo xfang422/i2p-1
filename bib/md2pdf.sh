5cc59ba1c78050507982ca53966be341e83e4a1e
Jon Reades <jon@reades.com>
2020-09-26 16:58:08 +0100

    Trying to sort out pandoc output.
    Former-commit-id: 0b35662ba55c114345117b5473942b95153b238b

---
+++ bib/md2pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+pandoc Syllabus.md \
+  -H ./bib/head.tex \
+  -H ./bib/chapter.tex \
+  -V documentclass=memoir \ 
+  --pdf-engine=xelatex \
+  --filter=pandoc-citeproc \
+  --metadata-file=metadata.yml \
+  --highlight-style=pygments \ 
+  -o Syllabus.pdf
